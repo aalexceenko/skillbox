947924ab42f8d5a0e805a1e5a92a85bd13513b67
aalexceenko <xnyxnymrik@bk.ru>
2020-06-23 11:59:03 +0300

    added js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
-var btn = document.querySelector(button);
+const btn = document.querySelector('button');
 
 checkValidation = (evt) => {
-  var email = document.getElementById("mail");
+  const email = document.getElementById('mail');
 
     if (email.validity.typeMismatch) {
-      email.setCustomValidity("I expect an e-mail, darling!");
+      email.setCustomValidity("Введите почту, пожалуйста");
     } else {
       email.setCustomValidity("");
     }
 
+    const name = document.getElementById('name');
+
+    if (name.validity.typeMismatch) {
+      name.setCustomValidity("Введите имя, пожалуйста");
+    } else {
+      name.setCustomValidity("");
+    }
+
+    const tel = document.getElementById('tel');
+
+    if (tel.validity.typeMismatch) {
+      tel.setCustomValidity("Введите телефон, пожалуйста");
+    } else {
+      tel.setCustomValidity("");
+    }
+
+    const check = document.getElementById('check');
+
+
+    if(!check.checked) {
+      check.setCustomValidity("Подтвердите, что согласны с условиями, пожалуйста");
+      check.focus();
+    }
+
+
 }
 
 btn.addEventListener('click', checkValidation);
-
-console.log(9997);
